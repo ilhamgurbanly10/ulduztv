7eb73f04e44ddc0712f9b03915282d68aebfacc3
ilhamgurbanly10 <Ilham_gurbanly@mail.ru>
2022-04-19 17:07:11 +0500

    first commit

--- js/body.js
+++ js/body.js
@@ -1,20 +1,61 @@
 
-// calling-functions
+// head-simple-slider
 
 flashSimpleSlider("#headSimpleSlider", {
 	autoplay: true,
-	draggable: false
+	buttons: true
 });
 
-flashSimpleSlider("#bottomSimpleSlider", {
-	autoplay: true,
-	buttons: true,
-	dots: false
+// the-end-of-head-simple-slider
+
+
+
+// page-first-slider
+
+$('.page-first-slider').slick({
+  dots: false,
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  speed: 300,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2
+      }
+    }
+  ]  
 });
 
-flashScrollIndicator();
+// page-first-slider
 
-flashLiveChat();
 
-// the-end-of-calling-functions
 
+// page-second-slider
+
+$('.page-second-slider').slick({
+  dots: false,
+  infinite: true,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  speed: 300,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 599,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]  
+});
+
+// page-second-slider
